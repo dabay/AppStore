43438d443f05e45e908344e2762793408626c4c3
dabay <dabay.wong@gmail.com>
2015-04-17 16:20:33 +0100

    update

--- AppStore/AppStore/items.py
+++ AppStore/AppStore/items.py
@@ -6,15 +6,14 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.contrib.loader.processor import TakeFirst
 
 
 class AppstoreItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    app_id = scrapy.Field(output_processor=TakeFirst())
-    name = scrapy.Field(output_processor=TakeFirst())
-    url = scrapy.Field(output_processor=TakeFirst())
+    app_id = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
 
 
 class CategoryItem(scrapy.Item):
